fa8cb2e1f77495b27f5670ad789df43a220e26ed
Aleeloppes <aleeloppes@hotmail.com>
2022-10-07 19:32:21 -0300

    primeiro passo

---
+++ js/main.js
@@ -0,0 +1,19 @@
+
+/*$(document).ready(function(){
+  $('.slider-principal').slick({
+    setting-name: setting-value
+  });
+
+}); */
+
+$('.slider-principal').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    autoplay: true,
+    autoplaySpeed: 2000
+  });
+        
+/*$('.slider-principal').slick();*/
