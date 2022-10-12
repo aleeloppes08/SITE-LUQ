7dec3d1af4d9d0b3c0d160652edaa5f04e0b07dd
Aleeloppes <aleeloppes@hotmail.com>
2022-10-12 01:44:21 -0300

    Segundo passo

--- js/main.js
+++ js/main.js
@@ -1,19 +1,17 @@
 
-/*$(document).ready(function(){
-  $('.slider-principal').slick({
-    setting-name: setting-value
-  });
-
-}); */
-
-$('.slider-principal').slick({
+ $('.slider-principal').slick({
     dots: true,
     infinite: true,
     speed: 500,
     fade: true,
     cssEase: 'linear',
     autoplay: true,
-    autoplaySpeed: 2000
-  });
+    autoplaySpeed: 2000,
+    useTransform: true
+  }); 
         
-/*$('.slider-principal').slick();*/
+  
+/*$('.slider-principal').slick({
+ dots: true 
+}); 
+*/
